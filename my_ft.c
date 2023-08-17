5f8d61bba0269004ebd20caf0f0f8626a5c1f177
mpruvot <132161864+mpruvot@users.noreply.github.com>
2023-08-17 12:18:04 +0200

    màj

---
+++ my_ft.c
@@ -0,0 +1,80 @@
+#include "my_ft.h"
+
+// Outputs a character
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+// Calculates string length
+int my_strlen(char *str)
+{
+    int len = 0;
+
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    return len;
+}
+
+// Outputs a string
+void my_putstr(char *str) 
+{
+    int i = 0;
+    
+    while (str[i] != '\0') 
+    {
+        my_putchar(str[i]);
+        i++;
+    }
+
+    my_putchar('\n');
+}
+
+// Outputs an integer
+void my_putnbr(int nb)
+{
+    long number = nb;
+
+    if (number < 0) 
+    {
+        my_putchar('-'); 
+        number = -number; 
+    }
+  
+    if (number >= 0 && number <= 9)
+    {
+        my_putchar(number + '0');   
+    }
+    else
+    {
+        my_putnbr(number / 10);
+        my_putchar((number % 10) + '0');
+    }
+}
+
+// Copies one string to another
+char *my_strcpy(char *dest, char *src)
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';
+    return dest;
+}
+
+//Verify if it's a space
+int is_space(char c)
+{
+    if (c == ' ')
+        return 1;
+    else
+        return 0;
+}
---
+++ my_struct.h
@@ -0,0 +1,25 @@
+#ifndef MY_STRUCT_H
+#define MY_STRUCT_H
+
+#include <unistd.h>
+
+// General Single Linked List
+typedef struct s_list {
+  void *content;
+  struct s_list *next;
+} t_list;
+
+
+// Linked list to Store User infos
+typedef struct s_user {
+  char *type;
+  char *name;
+  struct s_list *next;
+} t_user;
+
+// Fonction to print a list
+void print_list(t_user *lst);
+
+#endif // MY_STRUCT_H
+// End of the #ifndef directive. Everything between #ifndef and #endif
+// will be ignored if MY_STRUCT_H is already defined, thus preventing multiple definitions.
